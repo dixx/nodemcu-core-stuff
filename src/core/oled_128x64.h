4ebc270a3659cb36e4ec5187fab9f8060e60c39e
dixx <jenzdixx@googlemail.com>
2018-07-19 21:48:20 +0200

    Add OLED display

---
+++ src/core/oled_128x64.h
@@ -0,0 +1,33 @@
+#ifndef OLED_DISPLAY_H
+#define OLED_DISPLAY_H
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+namespace oled_display {
+    Adafruit_SSD1306 display(-1);
+
+    void init() {
+        display.begin();
+        display.clearDisplay();
+        display.display();
+    }
+
+    void splashScreen() {
+        display.drawPixel(0, 0, WHITE);
+        display.drawPixel(127, 0, WHITE);
+        display.drawPixel(0, 63, WHITE);
+        display.drawPixel(127, 63, WHITE);
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        display.setCursor(28, 30);
+        display.println("Guten Tag!");
+        display.display();
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include "Arduino.h"
 #include "core/webupdate.h"
 #include "core/onboard_led.h"
+#include "core/oled_128x64.h"
 #include "fun/rythm_blink.h"
 #include "my_secrets.h"
 
 RythmBlink rythm(
-    "++.... +....+ +..... ......", // the 'bassline'
-    true, // repeat?
+    "++.... +....+ +..... ...... ++.... +....+ +..... ......", // the 'bassline'
+    false, // repeat?
     10, // LED glow time in ms
     100 // the shortest tone in the song (interval)
 );
 
 void setup() {
-    onboard_led::init();
     Serial.begin(115200);
     Serial.println("");
     WiFiConnection(MY_SSID, MY_WLAN_PASSWORD, MY_NODEMCU_WEB_USERNAME, MY_NODEMCU_WEB_PASSWORD);
+    onboard_led::init();
+    oled_display::init();
+    oled_display::splashScreen();
 }
 
 void loop() {
